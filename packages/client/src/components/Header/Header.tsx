1c58d16b572cf462f4872936b7f96a4434fc2036
zubastu <79757684+zubastu@users.noreply.github.com>
2023-08-15 21:11:07 +0300

    zubastu/16bit-34-сверстать-страницу-профиля

--- packages/client/src/components/Header/Header.tsx
+++ packages/client/src/components/Header/Header.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import styles from './styles.module.css'
 
 const Header = () => {
-  return <nav className={styles.header}></nav>
+  return (
+    <nav className={styles.header}>
+      <ul></ul>
+    </nav>
+  )
 }
 
 export default Header
