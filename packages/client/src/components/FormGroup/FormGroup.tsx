47ecd876b384c62ac84edfe24983b07bdb06aaa3
Полина Куксова <pakuksova@gmail.com>
2023-08-17 23:39:46 +0300

    add inputs to register page

--- packages/client/src/components/FormGroup/FormGroup.tsx
+++ packages/client/src/components/FormGroup/FormGroup.tsx
@@ -1,10 +1,7 @@
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
-import spriteSvg from '../../assets/images/blueSprite.png';
-import Input from '../InputWithLabel/InputWithLabel';
 import SubmitButton from '../Button/Button';
-import { ValidationEntry } from '../../commonTypes';
 import styles from './styles.module.scss';
 import Sprite from '../FormSprite/FormSprite';
 
@@ -13,61 +10,33 @@ type FormValues = {
   password: string;
 };
 
-const validation: Record<string, ValidationEntry> = {
-  login: {
-    required: 'Login is required',
-    minLength: { value: 3, message: 'at least 3 characters' },
-    maxLength: { value: 20, message: 'no more than 20 characters' },
-    pattern: {
-      value: /^[a-zA-Z0-9-]{3,20}$/,
-      message: `Only Latin letters, digits, but not composed solely of them, without spaces, 
-      without special characters (hyphens and underscores are allowed)`,
-    },
-  },
-  password: {
-    required: 'Password is required',
-    minLength: { value: 8, message: 'at least 8 characters' },
-    maxLength: { value: 40, message: 'no more than 40 characters' },
-    pattern: {
-      value: /^(?=.*[A-Z])(?=.*\d).{8,40}$/,
-      message: 'At least one uppercase letter and one digit are required',
-    },
-  },
-};
+interface FormGroupProps {
+  title: string;
+  spriteImg: string;
+  bottomText: string;
+  link: string;
+  children: React.ReactNode | React.ReactNode[];
+}
 
-const FormGroup = () => {
+const FormGroup = (props: FormGroupProps) => {
   const methods = useForm<FormValues>();
   const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
   return (
     <article className={styles.formContainer}>
-      <h2 className={styles.title}>Login</h2>
+      <h2 className={styles.title}>{props.title}</h2>
       <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <Input
-            label="Login"
-            type="text"
-            name="login"
-            placeholder="Enter your login"
-            autoFocus={true}
-            validation={validation.login}
-          />
-          <Input
-            label="Password"
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            validation={validation.password}
-          />
+        <form className={styles.form} onSubmit={methods.handleSubmit(onSubmit)}>
+          {props.children}
         </form>
       </FormProvider>
       <section className={styles.section}>
-        <Sprite spriteImg={spriteSvg} />
-        <SubmitButton label="Login" />
+        <Sprite spriteImg={props.spriteImg} />
+        <SubmitButton label={props.title} />
         <p>
-          Don`t have an account yet?
+          {props.bottomText}
           <span>&nbsp;</span>
           <Link className={styles.link} to="/register">
-            Register
+            {props.link}
           </Link>
         </p>
       </section>
--- packages/client/src/pages/Register/Register.tsx
+++ packages/client/src/pages/Register/Register.tsx
@@ -1,10 +1,65 @@
 import styles from './styles.module.scss';
 import FormGroup from '../../components/FormGroup/FormGroup';
+import spriteSvg from '../../assets/images/purple_ghost.png';
+import Input from '../../components/InputWithLabel/InputWithLabel';
 
 const Register = () => {
   return (
     <div className={styles.registerPage}>
-      <FormGroup />
+      <FormGroup
+        title="Register"
+        spriteImg={spriteSvg}
+        bottomText="Already have an account?"
+        link="Login"
+      >
+        <Input
+          label="Email"
+          type="email"
+          name="email"
+          placeholder="pochta@yandex.ru"
+          autoFocus={true}
+        />
+        <Input
+          label="Login"
+          type="login"
+          name="login"
+          placeholder="ivanivanov"
+          autoFocus={true}
+        />
+        <Input
+          label="Name"
+          type="text"
+          name="name"
+          placeholder="Ivan"
+          autoFocus={true}
+        />
+        <Input
+          label="Surname"
+          type="text"
+          name="surname"
+          placeholder="Ivanov"
+          autoFocus={true}
+        />
+        <Input
+          label="Phone"
+          type="phone"
+          name="phone"
+          placeholder="+79098087766"
+          autoFocus={true}
+        />
+        <Input
+          label="Password"
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+        />
+        <Input
+          label="Password"
+          type="password"
+          name="password"
+          placeholder="Repeat password"
+        />
+      </FormGroup>
     </div>
   );
 };
