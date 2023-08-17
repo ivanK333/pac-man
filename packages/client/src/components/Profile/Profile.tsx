9e1e63bc766287eb550a64cb3b9715a69fa95549
zubastu <79757684+zubastu@users.noreply.github.com>
2023-08-17 15:30:31 +0300

    Переделал стили для компонента Profile

--- packages/client/src/components/Profile/Profile.tsx
+++ packages/client/src/components/Profile/Profile.tsx
@@ -1,11 +1,14 @@
-import styles from './styles.module.css';
+import styles from './styles.module.scss';
 import Avatar from '../Avatar/Avatar';
 
 const Profile = () => {
   return (
     <div className={styles.container}>
       <div className={styles.contentContainer}>
-        <Avatar />
+        <div className={styles.avatarContainer}>
+          <Avatar />
+          <p className={styles.username}>Username</p>
+        </div>
         <div className={styles.formContainer}></div>
       </div>
     </div>
