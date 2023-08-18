6b7b924c5f8869d8eecb14a06e60ed8df5dc3d00
Midkhat <midkhat@birdi.com.au>
2023-08-18 21:30:29 +1000

    fixing weirg Casing error

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -56,8 +56,6 @@ module.exports = {
     'react/jsx-uses-react': 0,
     'react/react-in-jsx-scope': 0,
     'react/display-name': 1,
-    // adding this line as get
-    // 'import/no-unresolved': [2, { caseSensitive: false }],
   },
   settings: {
     react: {
