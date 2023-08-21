256581442c29cc235774d23291767899427fc25e
zubastu <79757684+zubastu@users.noreply.github.com>
2023-08-21 12:12:56 +0300

    Поправил верстку

--- packages/client/src/routes/Main/Main.tsx
+++ packages/client/src/routes/Main/Main.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router';
 
+import styles from './styles.module.scss';
 import Header from '../../components/Header/Header';
 import Profile from '../../components/Profile/Profile';
 import Leaderboard from '../../components/Leaderboard/Leaderboard';
@@ -10,17 +11,20 @@ export const Main = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path={ROUTES.main.profile} element={<Profile />} />
-        <Route path={ROUTES.main.root} element={<div>Game</div>} />
-        <Route path={ROUTES.main.gameOver} element={<GameOver />} />
-        <Route path={ROUTES.main.forum} element={<div>Forum</div>} />
-        <Route path={ROUTES.main.lead} element={<Leaderboard />} />
-        <Route
-          path="/*"
-          element={<Navigate replace to={ROUTES.error.notFound} />}
-        />
-      </Routes>
+      <main className={styles.main}>
+        <Routes>
+          <Route path={ROUTES.main.profile} element={<Profile />} />
+          <Route path={ROUTES.main.root} element={<div>Game</div>} />
+          <Route path={ROUTES.main.gameOver} element={<GameOver />} />
+          <Route path={ROUTES.main.forum} element={<div>Forum</div>} />
+          <Route path={ROUTES.main.lead} element={<Leaderboard />} />
+
+          <Route
+            path="/*"
+            element={<Navigate replace to={ROUTES.error.notFound} />}
+          />
+        </Routes>
+      </main>
     </>
   );
 };
