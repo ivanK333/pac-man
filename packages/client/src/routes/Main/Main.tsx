26fd448ef991da8a02c8ae25a9e12e1c81de0e8e
Midkhat <midkhat@birdi.com.au>
2023-08-23 22:12:08 +1000

    merged sprint_5

--- packages/client/src/routes/Main/Main.tsx
+++ packages/client/src/routes/Main/Main.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.scss';
 import Header from '../../components/Header/Header';
 import GameOver from '../../pages/GameOver/GameOver';
 import { ROUTES } from '../../constants/routes';
+import GameField from '../../pages/Game/Game';
 import Profile from '../../pages/Profile/Profile';
 import Leaderboard from '../../pages/Leaderboard/Leaderboard';
 
@@ -14,11 +15,10 @@ export const Main = () => {
       <main className={styles.main}>
         <Routes>
           <Route path={ROUTES.main.profile} element={<Profile />} />
-          <Route path={ROUTES.main.root} element={<div>Game</div>} />
+          <Route path={ROUTES.main.root} element={<GameField />} />
           <Route path={ROUTES.main.gameOver} element={<GameOver />} />
           <Route path={ROUTES.main.forum} element={<div>Forum</div>} />
           <Route path={ROUTES.main.lead} element={<Leaderboard />} />
-
           <Route
             path="/*"
             element={<Navigate replace to={ROUTES.error.notFound} />}
