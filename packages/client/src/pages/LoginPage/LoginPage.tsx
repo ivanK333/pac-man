8fa96faef49577d4af8a683b284b9876655c31ce
Midkhat <midkhat@birdi.com.au>
2023-08-24 23:29:27 +1000

    merged

--- packages/client/src/pages/LoginPage/LoginPage.tsx
+++ packages/client/src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { useNavigate } from 'react-router';
+// import { useNavigate } from 'react-router';
 import { SubmitHandler } from 'react-hook-form';
 
 import AuthController from '../../controllers/AuthController';
