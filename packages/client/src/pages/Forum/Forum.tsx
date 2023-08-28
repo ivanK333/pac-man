b332debce46f433822455500feea6598223a8e99
zubastu <79757684+zubastu@users.noreply.github.com>
2023-08-28 18:57:40 +0300

    Оптимизировал код

--- packages/client/src/pages/Forum/Forum.tsx
+++ packages/client/src/pages/Forum/Forum.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router';
-import { NavLink, useMatch } from 'react-router-dom';
+import { useMatch } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 import { ROUTES } from '../../constants/routes';
@@ -7,52 +7,12 @@ import Topics from '../Topics/Topics';
 
 const Forum = () => {
   const matchForumRoot = useMatch({ path: ROUTES.main.forum.root, end: true });
-  const matchTopic = useMatch({ path: 'forum/topic', end: false });
   const isForumRoot = Boolean(matchForumRoot);
-  const isTopic = Boolean(matchTopic);
   return (
     <>
       <div
-        className={
-          isForumRoot || isTopic ? styles.container : styles.containerWithImage
-        }
+        className={isForumRoot ? styles.container : styles.containerWithImage}
       >
-        {!isTopic && (
-          <ul className={styles.forumNavigation}>
-            <li>
-              <NavLink
-                to="/forum/"
-                className={({ isActive }) =>
-                  isActive ? styles.linkActive : styles.link
-                }
-              >
-                Forum
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="about"
-                className={({ isActive }) =>
-                  isActive ? styles.linkActive : styles.link
-                }
-              >
-                About us
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink
-                to="tech"
-                className={({ isActive }) =>
-                  isActive ? styles.linkActive : styles.link
-                }
-              >
-                Technologies
-              </NavLink>
-            </li>
-          </ul>
-        )}
-
         {isForumRoot ? <Topics /> : <Outlet />}
       </div>
     </>
--- packages/client/src/pages/Tech/Tech.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './styles.module.scss';
-
-const Tech = () => {
-  return <div></div>;
-};
-
-export default Tech;
