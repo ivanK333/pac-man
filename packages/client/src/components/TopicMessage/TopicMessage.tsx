ea86f3b068aa404c03d70520139c8935cb02648b
zubastu <79757684+zubastu@users.noreply.github.com>
2023-08-29 20:27:20 +0300

    изменил внешний вид сообщения в теме, добавил форму для комментариев к сообщению

--- packages/client/src/components/TopicMessage/TopicMessage.tsx
+++ packages/client/src/components/TopicMessage/TopicMessage.tsx
@@ -1,7 +1,45 @@
 import styles from './styles.module.scss';
+import AvatarImage from '../AvatarImage/AvatarImage';
+import TopicForm from '../TopicForm/TopicForm';
+import { TTopicForm } from '../../pages/Topic/Topic';
 
-const TopicMessage = () => {
-  return <div></div>;
+type TTopicMessageProps = {
+  image: string;
+  username: string;
+  message: string;
+  time: string;
+};
+
+const TopicMessage: React.FC<TTopicMessageProps> = ({
+  image,
+  username,
+  message,
+  time,
+}) => {
+  const handleSubmit = (data: TTopicForm) => {
+    console.log(data);
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.loginContainer}>
+        <p className={styles.username}>{username}</p>
+        <p className={styles.time}>{time}</p>
+      </div>
+      <div className={styles.userContainer}>
+        <AvatarImage image={image} />
+        <div className={styles.messageContainer}>
+          <p className={styles.message}>{message}</p>
+          <div className={styles.comments}>
+            <TopicForm
+              placeholder="Enter a comment"
+              autoFocus={false}
+              onSubmit={handleSubmit}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TopicMessage;
--- packages/client/src/pages/Topic/Topic.tsx
+++ packages/client/src/pages/Topic/Topic.tsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from 'react-router';
 import styles from './styles.module.scss';
 import blueGhost from '../../assets/images/blueSprite.svg';
 import TopicForm from '../../components/TopicForm/TopicForm';
+import TopicMessage from '../../components/TopicMessage/TopicMessage';
 
 export type TTopicForm = { message: string };
 
@@ -27,12 +28,45 @@ const Topic = () => {
       </div>
 
       <div className={styles.messages}>
-        <div className={styles.messageList}>
+        <div className={styles.formContainer}>
           <TopicForm
             onSubmit={handleSubmit}
             placeholder="Enter your message"
             autoFocus={true}
           />
+        </div>
+        <div className={styles.messageList}>
+          <TopicMessage
+            message="test"
+            image=""
+            time="12:33"
+            username="20lettersloginlength"
+          />
+          <TopicMessage
+            message="test"
+            image=""
+            time="12:33"
+            username="20LETTERSLOGINLENGTH"
+          />
+          <TopicMessage
+            /* eslint-disable-next-line max-len */
+            message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            image=""
+            time="12:33"
+            username="test"
+          />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
+          <TopicMessage message="test" image="" time="12:33" username="test" />
         </div>
       </div>
     </div>
