38926ee4009e0d5bb08ecff99eb4a538a0234e49
Midkhat <midkhat@birdi.com.au>
2023-08-31 00:38:20 +1000

    started

---
+++ packages/client/src/engine/AnimatedCharacters/pacman.tsx
@@ -0,0 +1,7 @@
+export const drawPacman2 = (ctx: CanvasRenderingContext2D) => {
+  ctx.save();
+  // Issue drawing commands, assuming some 0,0 center and an unrotated bug
+  // Use the current time, or some frame counter, to change how things are drawn
+
+  ctx.restore();
+};
