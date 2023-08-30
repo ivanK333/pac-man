6f2c7a59dd70d7b3bf5120c66ecf808c9db12fa0
zubastu <79757684+zubastu@users.noreply.github.com>
2023-08-30 15:36:36 +0300

    Подправил роут для игры

--- packages/client/src/routes/Main/Main.tsx
+++ packages/client/src/routes/Main/Main.tsx
@@ -2,12 +2,12 @@ import { Routes, Route, Navigate } from 'react-router';
 
 import styles from './styles.module.scss';
 import Header from '../../components/Header/Header';
-import GameOver from '../../pages/GameOver/GameOver';
 import { ROUTES } from '../../constants/routes';
 import Profile from '../../pages/Profile/Profile';
 import Leaderboard from '../../pages/Leaderboard/Leaderboard';
 import Forum from '../../pages/Forum/Forum';
 import Topic from '../../pages/Topic/Topic';
+import Game from '../../pages/Game/Game';
 
 export const Main = () => {
   return (
@@ -16,11 +16,7 @@ export const Main = () => {
       <main className={styles.main}>
         <Routes>
           <Route path={ROUTES.main.profile} element={<Profile />} />
-          <Route path={ROUTES.main.root} element={<div>Game</div>} />
-          <Route
-            path={ROUTES.main.gameOver}
-            element={<GameOver restartGame={() => console.log('restart')} />}
-          />
+          <Route path={ROUTES.main.root} element={<Game />} />
           <Route path={ROUTES.main.forum.root} element={<Forum />}>
             <Route path={ROUTES.main.forum.topic} element={<Topic />} />
           </Route>
