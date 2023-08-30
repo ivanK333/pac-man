699070d403c104f6a3947c5f73ed96f3228c1a67
zubastu <79757684+zubastu@users.noreply.github.com>
2023-08-31 00:48:17 +0300

    обновление анимации

--- packages/client/src/pages/LoaderGame/LoaderGame.tsx
+++ packages/client/src/pages/LoaderGame/LoaderGame.tsx
@@ -1,19 +1,37 @@
+import { useState } from 'react';
+
 import styles from './styles.module.scss';
 
 const DOTS_NUMBER = 8;
 const LoaderGame = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.loaderContainer}>
-        <div className={styles.pacman} />
+  const [active, setIsActive] = useState(true);
 
-        <ul className={styles.dots}>
-          {new Array(DOTS_NUMBER).fill(null).map((_, index) => (
-            <li key={`dot-${index}`} className={styles.dot} />
-          ))}
-        </ul>
-      </div>
-    </div>
+  const animationToggle = () => {
+    setIsActive(false);
+    setTimeout(() => setIsActive(true), 5);
+  };
+  return (
+    <>
+      {active ? (
+        <div className={styles.container}>
+          <div
+            className={styles.loaderContainer}
+            onAnimationEnd={(e) => {
+              if (e.animationName === '_travel_su64p_1') {
+                animationToggle();
+              }
+            }}
+          >
+            <div className={styles.pacman} />
+            <ul className={styles.dots}>
+              {new Array(DOTS_NUMBER).fill(null).map((_, index) => (
+                <li key={`dot-${index}`} className={styles.dot} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
