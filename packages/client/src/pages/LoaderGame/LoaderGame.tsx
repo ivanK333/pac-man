9d47199c90dec305bf51c44b2f48be6e21b05b12
Полина Куксова <pakuksova@gmail.com>
2023-08-31 14:43:01 +0300

    revert changes

--- packages/client/src/pages/LoaderGame/LoaderGame.tsx
+++ packages/client/src/pages/LoaderGame/LoaderGame.tsx
@@ -4,34 +4,17 @@ import styles from './styles.module.scss';
 
 const DOTS_NUMBER = 8;
 const LoaderGame = () => {
-  const [active, setIsActive] = useState(true);
-
-  const animationToggle = () => {
-    setIsActive(false);
-    setTimeout(() => setIsActive(true), 5);
-  };
   return (
-    <>
-      {active ? (
-        <div className={styles.container}>
-          <div
-            className={styles.loaderContainer}
-            onAnimationEnd={(e) => {
-              if (e.animationName === '_travel_su64p_1') {
-                animationToggle();
-              }
-            }}
-          >
-            <div className={styles.pacman} />
-            <ul className={styles.dots}>
-              {new Array(DOTS_NUMBER).fill(null).map((_, index) => (
-                <li key={`dot-${index}`} className={styles.dot} />
-              ))}
-            </ul>
-          </div>
-        </div>
-      ) : null}
-    </>
+    <div className={styles.container}>
+      <div className={styles.loaderContainer}>
+        <div className={styles.pacman} />
+        <ul className={styles.dots}>
+          {new Array(DOTS_NUMBER).fill(null).map((_, index) => (
+            <li key={`dot-${index}`} className={styles.dot} />
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
