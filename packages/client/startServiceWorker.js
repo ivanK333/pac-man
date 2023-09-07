2645fcc0218caa068c68b8fca5b6cf383c1eb09a
Полина Куксова <pakuksova@gmail.com>
2023-09-07 14:32:45 +0300

    delete spaces

--- packages/client/startServiceWorker.js
+++ packages/client/startServiceWorker.js
@@ -1,13 +1,12 @@
 window.addEventListener('load', async () => {
-    try {
-      const registration = await navigator.serviceWorker.register('./sw.js');
+  try {
+    const registration = await navigator.serviceWorker.register('./sw.js');
 
-      console.log(
-        'ServiceWorker registration successful with scope: ',
-        registration.scope,
-      );
-    } catch (error) {
-      console.log('ServiceWorker registration failed: ', error);
-    }
-
+    console.log(
+      'ServiceWorker registration successful with scope: ',
+      registration.scope,
+    );
+  } catch (error) {
+    console.log('ServiceWorker registration failed: ', error);
+  }
 });
