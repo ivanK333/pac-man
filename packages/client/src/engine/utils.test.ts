cc6425e8edac80e8f1d47d8d65cd3cbe2bc3c012
zubastu <79757684+zubastu@users.noreply.github.com>
2023-09-11 18:02:04 +0300

    Тесты на функцию getObstacles

--- packages/client/src/engine/utils.test.ts
+++ packages/client/src/engine/utils.test.ts
@@ -1,4 +1,4 @@
-import { updateMap } from './GameCanvas';
+import { getObstacles, updateMap } from './GameCanvas';
 import { MapElements } from './config';
 
 describe('Тест логики изменения карты', () => {
@@ -14,5 +14,43 @@ describe('Тест логики изменения карты', () => {
       updateMap(0, i, MapElements.NONE, map);
     }
     expect(map).toEqual(newMap);
+  });
+});
+
+describe('Тест функции getObstacles', () => {
+  test('Проверка в начальной позиции', () => {
+    const result = getObstacles(10, 1);
+    expect(result.left).toEqual(true);
+    expect(result.right).toEqual(true);
+    expect(result.up).toEqual(false);
+    expect(result.down).toEqual(false);
+    expect(result.stop).toEqual(false);
+  });
+
+  test('Проверка в [10, 7]', () => {
+    const result = getObstacles(10, 7);
+    expect(result.left).toEqual(true);
+    expect(result.right).toEqual(false);
+    expect(result.up).toEqual(true);
+    expect(result.down).toEqual(true);
+    expect(result.stop).toEqual(false);
+  });
+
+  test('Проверка в [15, 7]', () => {
+    const result = getObstacles(15, 7);
+    expect(result.left).toEqual(true);
+    expect(result.right).toEqual(true);
+    expect(result.up).toEqual(true);
+    expect(result.down).toEqual(false);
+    expect(result.stop).toEqual(false);
+  });
+
+  test('Проверка в [10, 10]', () => {
+    const result = getObstacles(10, 10);
+    expect(result.left).toEqual(false);
+    expect(result.right).toEqual(false);
+    expect(result.up).toEqual(false);
+    expect(result.down).toEqual(false);
+    expect(result.stop).toEqual(false);
   });
 });
