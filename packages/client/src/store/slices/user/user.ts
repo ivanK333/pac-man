15efa9d5bac7fbd7b7a1190dc42339ccf11db47e
ivan333ka <ivan333ka@yandex.ru>
2023-09-12 16:14:45 +0700

    added user to slice

--- packages/client/src/store/slices/user/user.ts
+++ packages/client/src/store/slices/user/user.ts
@@ -1,23 +1,24 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import { UserState } from './types';
+import { User } from '../../../api/AuthAPI';
 
-const initialState: UserState = {
-  avatar: '',
-  display_name: '',
-  email: '',
+const initialState: User = {
+  id: 0,
   first_name: '',
-  id: '',
-  login: '',
-  phone: '',
   second_name: '',
+  display_name: '',
+  login: '',
+  email: '',
+  password: '',
+  phone: '',
+  avatar: '',
 };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    addUser: (_, action: PayloadAction<UserState>) => action.payload,
+    addUser: (_, action: PayloadAction<User>) => action.payload,
     delUser: () => initialState,
   },
 });
