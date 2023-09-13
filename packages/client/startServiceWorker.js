66e5c8e0d9410c93628bea50267f7f49f9ae2b8c
Полина Куксова <pakuksova@gmail.com>
2023-09-13 10:53:04 +0300

    change path for sw.js

--- packages/client/startServiceWorker.js
+++ packages/client/startServiceWorker.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', async () => {
   if ('serviceWorker' in navigator) {
     try {
-      const registration = await navigator.serviceWorker.register('./sw.js');
+      const registration = await navigator.serviceWorker.register('sw.js', { scope: "." });
 
       console.log(
         'ServiceWorker registration successful with scope: ',
