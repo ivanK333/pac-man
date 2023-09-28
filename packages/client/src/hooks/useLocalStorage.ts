451767b7a7c1b5ae55dc9c1181b4356fe8ca63e2
zubastu <79757684+zubastu@users.noreply.github.com>
2023-09-28 15:34:08 +0300

    исправил работу хука по условию, добавил возврат из эффекта если нет window и начальное значение при его наличии

--- packages/client/src/hooks/useLocalStorage.ts
+++ packages/client/src/hooks/useLocalStorage.ts
@@ -1,21 +1,21 @@
 import { useState, useEffect } from 'react';
-
 export function useReadLocalStorage(nameItem: string) {
-  if (typeof window === 'object' || typeof window !== 'undefined') {
-    const [value, setValue] = useState(localStorage.getItem(nameItem));
+  const hasWindow = typeof window === 'object' || typeof window !== 'undefined';
+  const [value, setValue] = useState(
+    hasWindow ? localStorage.getItem(nameItem) : null,
+  );
 
-    useEffect(() => {
-      const storageListener = () => {
-        console.log('set');
-        setValue(localStorage.getItem(nameItem));
-      };
+  useEffect(() => {
+    if (!hasWindow) return;
+    const storageListener = () => {
+      setValue(localStorage.getItem(nameItem));
+    };
 
-      window.addEventListener('storage', storageListener, false);
-      return () => {
-        window.removeEventListener('storage', storageListener, false);
-      };
-    }, []);
+    window.addEventListener('storage', storageListener, false);
+    return () => {
+      window.removeEventListener('storage', storageListener, false);
+    };
+  }, []);
 
-    return value;
-  }
+  return value;
 }
