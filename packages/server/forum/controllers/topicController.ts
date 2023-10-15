3a77b9f1a31332ad204a9917d5f15f981b4bd8a9
zubastu <79757684+zubastu@users.noreply.github.com>
2023-10-15 20:51:21 +0300

    поправил ответ ручки для топиков

--- packages/server/forum/controllers/topicController.ts
+++ packages/server/forum/controllers/topicController.ts
@@ -7,8 +7,8 @@ const getTopics = (_: Request, res: Response) => {
   TopicModel.findAll({
     order: [[Sequelize.col('createdAt'), 'DESC']],
   })
-    .then(() => {
-      res.status(200).json(res.locals.user);
+    .then((topics) => {
+      res.status(200).json(topics);
     })
     .catch(() => {
       res.status(400).json({ message: 'Bad request' });
