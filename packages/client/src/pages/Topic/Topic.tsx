54def9fced3f53f3c729be3336b1e4eee5d50904
Midkhat <midkhat@gmail.com>
2023-10-16 20:42:47 +1100

    client API

--- packages/client/src/pages/Topic/Topic.tsx
+++ packages/client/src/pages/Topic/Topic.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.scss';
 import blueGhost from '../../assets/images/blueSprite.svg';
 import TopicForm from '../../components/TopicForm/TopicForm';
 import TopicMessage from '../../components/TopicMessage/TopicMessage';
-import { TTopic, TTopicWithMEssages, forumAPI } from '../../api';
+import { TTopicWithMEssages, forumAPI } from '../../api';
 
 export type TTopicForm = { message: string };
 
