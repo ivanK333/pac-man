75bccf4b80532fbc2b4e5dac06f93d5fad6b5c07
Midkhat <midkhat@gmail.com>
2023-10-16 21:03:18 +1100

    commented out init.ts

--- packages/server/forum/init.ts
+++ packages/server/forum/init.ts
@@ -1,15 +1,16 @@
-import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
+// import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
 import dotenv from 'dotenv';
 
-/** Этот файл, за исключением использования sql тригеров, создан другим участником команды */
+/** Этот файл, за исключением использования sql тригеров, создан другим участником команды.
+ * Оставлен для того, что показать как используются тригеры */
 
 // import CommentModel from './models/commentModel';
 // import TopicModel from './models/topicModel';
 // import MessageModel from './models/messageModel';
-import {
-  registerCommentsCountQuery,
-  registerMessagesCountQuery,
-} from './triggers';
+// import {
+//   registerCommentsCountQuery,
+//   registerMessagesCountQuery,
+// } from './triggers';
 
 dotenv.config();
 
@@ -19,26 +20,26 @@ POSTGRES_PASSWORD=postgres
 POSTGRES_HOST=localhost
 POSTGRES_DB=forum
 POSTGRES_PORT=5432;*/
-const sequelizeOptions: SequelizeOptions = {
-  host: process.env.POSTGRES_HOST!,
-  port: Number(process.env.POSTGRES_PORT!),
-  username: String(process.env.POSTGRES_USER!),
-  password: String(process.env.POSTGRES_PASSWORD!),
-  database: process.env.POSTGRES_DB!,
-  dialect: 'postgres',
-  logging: (msg) => console.log(msg),
-};
-export const sequelize = new Sequelize(sequelizeOptions);
+// const sequelizeOptions: SequelizeOptions = {
+//   host: process.env.POSTGRES_HOST!,
+//   port: Number(process.env.POSTGRES_PORT!),
+//   username: String(process.env.POSTGRES_USER!),
+//   password: String(process.env.POSTGRES_PASSWORD!),
+//   database: process.env.POSTGRES_DB!,
+//   dialect: 'postgres',
+//   logging: (msg) => console.log(msg),
+// };
+// export const sequelize = new Sequelize(sequelizeOptions);
 // sequelize.addModels([CommentModel, TopicModel, MessageModel]);
-export async function dbConnect() {
-  try {
-    await sequelize.authenticate(); // Проверка аутентификации в БД
-    await sequelize.sync(); // Синхронизация базы данных
-    console.log('Connection has been established successfully.');
+// export async function dbConnect() {
+//   try {
+//     await sequelize.authenticate(); // Проверка аутентификации в БД
+//     await sequelize.sync(); // Синхронизация базы данных
+//     console.log('Connection has been established successfully.');
 
-    await sequelize.query(registerCommentsCountQuery);
-    await sequelize.query(registerMessagesCountQuery);
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
+//     await sequelize.query(registerCommentsCountQuery);
+//     await sequelize.query(registerMessagesCountQuery);
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// }
