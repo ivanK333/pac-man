e1108a17058221a33c82516117bf9fd6155dd991
PolinaKuksova <pakuksova@gmail.com>
2023-10-18 22:16:25 +0300

    update forum

--- packages/client/src/pages/Forum/Forum.tsx
+++ packages/client/src/pages/Forum/Forum.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Outlet } from 'react-router';
 import { useMatch } from 'react-router-dom';
@@ -6,9 +6,10 @@ import classNames from 'classnames';
 
 import styles from './styles.module.scss';
 import { ROUTES } from '../../constants/routes';
-import TopicsList from '../TopicsList/TopicsList';
 import Modal from '../../components/Modal/Modal';
 import TopicCreationForm from '../../components/TopicCreationForm/TopicCreationForm';
+import { TCreateTopic, TTopic, forumAPI } from '../../api';
+import TopicItem from '../../components/TopicItem/TopicItem';
 import { useReadLocalStorage } from '../../hooks/useLocalStorage';
 
 const Forum = () => {
@@ -20,12 +21,42 @@ const Forum = () => {
   const isLightTheme = useReadLocalStorage('isLightTheme');
   const availableChangeThemeToDark = isLightTheme === 'true';
 
+  const { getTopics, createTopic } = forumAPI();
+
+  const [topics, setTopics] = useState<TTopic[]>([]);
+
   const handleOpenModal = () => {
     setIsOpenModal(true);
   };
 
   const handleCloseModal = () => {
     setIsOpenModal(false);
+  };
+
+  useEffect(() => {
+    console.log('refresh');
+    const getTopicsInfo = async () => {
+      const res = await getTopics();
+      if (res?.data) {
+        setTopics(res.data);
+      }
+    };
+
+    // const unlisten = history.listen(() => {
+    getTopicsInfo();
+    // });
+
+    // return () => {
+    //   unlisten();
+    // };
+  }, []);
+
+  const submitTopic = async (data: TCreateTopic) => {
+    const res = await createTopic(data);
+    if (res?.data) {
+      setTopics([res.data, ...topics]);
+      handleCloseModal();
+    }
   };
 
   return (
@@ -36,14 +67,32 @@ const Forum = () => {
         })}
       >
         {isForumRoot ? (
-          <TopicsList handleOpenModal={handleOpenModal} />
+          <div className={styles.listContainer}>
+            <div className={styles.topicList}>
+              <div className={styles.buttonContainer}>
+                <button onClick={handleOpenModal} className={styles.button}>
+                  Create a topic &gt;
+                </button>
+              </div>
+              {topics.length === 0 && <p>No topics yet</p>}
+
+              {topics.length > 0 &&
+                topics.map((topic) => (
+                  <TopicItem topic={topic} key={topic.id} />
+                ))}
+            </div>
+          </div>
         ) : (
           <Outlet />
         )}
       </div>
+
       {isOpenModal && (
         <Modal>
-          <TopicCreationForm handleCloseModal={handleCloseModal} />
+          <TopicCreationForm
+            onSubmit={submitTopic}
+            handleCloseModal={handleCloseModal}
+          />
         </Modal>
       )}
     </>
