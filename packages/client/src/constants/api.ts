ec163d4efd9a1cbf520103ed96f0886245b893ee
zubastu <79757684+zubastu@users.noreply.github.com>
2023-11-02 01:19:44 +0300

    исправил url's запросов

--- packages/client/src/constants/api.ts
+++ packages/client/src/constants/api.ts
@@ -1,6 +1,5 @@
-export const API_URL = 'https://16bitgames.ya-praktikum.tech/api/v2';
+export const API_URL = 'https://16bitgames.ya-praktikum.tech/api/v2/';
 
-export const RESOURCES_URL = `${API_URL}/resources`;
+export const RESOURCES_URL = `https://ya-praktikum.tech/api/v2/resources`;
 
-export const REDIRECT_URI =
-  typeof window === 'undefined' ? '' : window.location.origin;
+export const REDIRECT_URI = 'https://16bitgames.ya-praktikum.tech';
