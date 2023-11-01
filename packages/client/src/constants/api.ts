b3a9cbbc9ac6dca1fc0d77746dd5026de407bf6d
zubastu <79757684+zubastu@users.noreply.github.com>
2023-11-02 01:41:53 +0300

    исправил url's запросов

--- packages/client/src/constants/api.ts
+++ packages/client/src/constants/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://16bitgames.ya-praktikum.tech/api/v2/';
+export const API_URL = 'https://16bitgames.ya-praktikum.tech/api/v2';
 
 export const RESOURCES_URL = `https://ya-praktikum.tech/api/v2/resources`;
 
