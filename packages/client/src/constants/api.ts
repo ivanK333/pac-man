7e67fe861e1ee0147bb114974a5ce20eb7202dd6
zubastu <79757684+zubastu@users.noreply.github.com>
2023-11-02 13:21:31 +0300

    исправил Oauth

--- packages/client/src/constants/api.ts
+++ packages/client/src/constants/api.ts
@@ -4,4 +4,5 @@ export const OAUTH_API_URL = 'https://16bitgames.ya-praktikum.tech/api';
 
 export const RESOURCES_URL = `${API_URL}/resources`;
 
-export const REDIRECT_URI = 'https://16bitgames.ya-praktikum.tech';
+export const REDIRECT_URI =
+  typeof window === 'undefined' ? '' : window.location.origin;
