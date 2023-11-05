aa9a859abb8d31a49c49c98bb3f3a2823cb13733
Midkhat <midkhat@gmail.com>
2023-11-05 14:18:52 +1100

    constants urls

--- packages/client/src/api/forum/constants.ts
+++ packages/client/src/api/forum/constants.ts
@@ -1,1 +1,4 @@
-export const API_URL = 'https://16bitgames.ya-praktikum.tech/forum';
+// export const API_URL = 'https://16bitgames.ya-praktikum.tech/forum';
+
+const localhost = 'http://localhost:3005';
+export const API_URL = `${localhost}/forum`;
